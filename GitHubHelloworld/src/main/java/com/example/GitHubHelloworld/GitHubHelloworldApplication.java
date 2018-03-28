24494332f50adf1f641a1a6ef6d078685396fc43
shahimsha.shaik <shahimsha.shaik@YH1005554DT.Yash.com>
2018-03-28 18:14:37 +0530

    first commit by shahimsha

---
+++ GitHubHelloworld/src/main/java/com/example/GitHubHelloworld/GitHubHelloworldApplication.java
@@ -0,0 +1,12 @@
+package com.example.GitHubHelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubHelloworldApplication.class, args);
+	}
+}
